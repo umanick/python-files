78c510f60455b77d941165743c8432b72093a998
Upendran Manickam <upen@Upendrans-MacBook-Pro.local>
2018-05-14 22:32:39 -0700

    Reorganizing Learn Hard Way exercises

--- formatscript.py
+++ formatscript.py
@@ -6,10 +6,13 @@ out_file = open(op_filename, "w")
 with open("/Users/upen/Documents/github/pfiles/script.txt", "r") as ins:
     array = []
     dotab = False
-    for string in ins:
-        string = string.lstrip().rstrip()
-        if len(string) > 2:
-            line = string[string.index(' ')+1:len(string)]
+    for readline in ins:
+        readline = readline.lstrip().rstrip()
+        #print readline
+        #print len(readline)
+        #print readline.find(' ')
+        if readline.find(' ') > 0:
+            line = readline[readline.index(' ')+1:len(readline)]
             iselse = line.startswith('elif') | line.startswith('else')
             if dotab and not iselse:
                 out_file.write('\t')
